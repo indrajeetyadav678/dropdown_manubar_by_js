180b81b32070eada683f191caabebd97429ebd3b
indrajeetyadav678 <146438632+indrajeetyadav678@users.noreply.github.com>
2023-12-26 21:52:53 +0530

    Drop down manubar by html , CSS and javascript

---
+++ index.js
@@ -0,0 +1,381 @@
+function store() {
+    let st = `<div id="stor">
+    <ul id="store-block1">
+        <li id="block1-heading">Shop</li>
+        <li class="drop-block1"><a href="#" >Shop the Latest</a></li>
+        <li class="drop-block1"><a href="#">Mac</a></li>
+        <li class="drop-block1"><a href="#">ipad</a></li>
+        <li class="drop-block1"><a href="#">iphone</a></li>
+        <li class="drop-block1"><a href="#">Apple Watch</a></li>
+        <li class="drop-block1"><a href="#">Accessories</a></li>
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Quick Links</li>
+        <li class="drop-block2"><a href="#" >Shop holiday Gifts</a></li>
+        <li class="drop-block2"><a href="#">Find a Store</a></li>
+        <li class="drop-block2"><a href="#">Order Status</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">Shop Special stores</li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+        <li class="drop-block3"><a href="#">Business</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = st;
+    document.getElementById('offer_info').style.display = "none";
+}
+
+function mac() {
+    let ma = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Mac</li>
+        <li class="drop-block2"><a href="#" >Shop Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Mac</li>
+        <li class="drop-block3"><a href="#">Mac support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for Mac</a></li>
+        <li class="drop-block3"><a href="#">MacOS sonoma</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#"Continuity></a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#"Mac for Business></a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = ma;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function ipad() {
+    let ipa = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore ipad</li>
+        <li class="drop-block1"><a href="#" >Explore All ipad</a></li>
+        <li class="drop-block1"><a href="#">ipad Pro</a></li>
+        <li class="drop-block1"><a href="#">ipad Air</a></li>
+        <li class="drop-block1"><a href="#">ipad</a></li>
+        <li class="drop-block1"><a href="#">ipad mini</a></li>
+        <li class="drop-block1"><a href="#">ipad pancil</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Keyboards</a></li>
+        <li class="drop-block2"><a href="#">Compare ipad</a></li>
+        <li class="drop-block2"><a href="#">why ipad</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop ipad</li>
+        <li class="drop-block2"><a href="#" >Shop ipad</a></li>
+        <li class="drop-block2"><a href="#">ipad accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from ipad</li>
+        <li class="drop-block3"><a href="#">ipad support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for ipad</a></li>
+        <li class="drop-block3"><a href="#">ipadOS 17</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = ipa;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function iphone() {
+    let ipho = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore iPhone</li>
+        <li class="drop-block1"><a href="#" >Explore All iPhone</a></li>
+        <li class="drop-block1"><a href="#">iPhone 15 Pro</a></li>
+        <li class="drop-block1"><a href="#">iPhone 15</a></li>
+        <li class="drop-block1"><a href="#">iPhone 14</a></li>
+        <li class="drop-block1"><a href="#">iPhone 13</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">iPhone SE</a></li>
+        <li class="drop-block2"><a href="#">Compare iPhone</a></li>
+        <li class="drop-block2"><a href="#">Switch from Android</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop iPhone</li>
+        <li class="drop-block2"><a href="#" >Shop iPhone</a></li>
+        <li class="drop-block2"><a href="#">iPhone accessories</a></li>
+        <li class="drop-block2"><a href="#">Appl trade in</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from iPhone</li>
+        <li class="drop-block3"><a href="#">iPhone support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ iPhone</a></li>
+        <li class="drop-block3"><a href="#">iOS 17</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#"Appl></a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#">Wallet</a></li>
+        <li class="drop-block3"><a href="#">siri</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = ipho;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+
+function watch() {
+    let watc = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Watch</li>
+        <li class="drop-block1"><a href="#" >Explore All Apple Watch</a></li>
+        <li class="drop-block1"><a href="#">Apple Watch series 9</a></li>
+        <li class="drop-block1"><a href="#">Apple Watch Ultra 2</a></li>
+        <li class="drop-block1"><a href="#">Apple Watch SE</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Apple Watch Nike</a></li>
+        <li class="drop-block2"><a href="#">Compare Watch</a></li>
+        <li class="drop-block2"><a href="#">Why Apple Watch</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Watch</li>
+        <li class="drop-block2"><a href="#" >Shop Apple Watch</a></li>
+        <li class="drop-block2"><a href="#">Apple Watch accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Watch</li>
+        <li class="drop-block3"><a href="#">Apple Watch support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+</a></li>
+        <li class="drop-block3"><a href="#">WatchOS 10</a></li>
+        <li class="drop-block3"><a href="#">Apple by Apple</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = watc;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function airpods() {
+    let airp = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Airpods</li>
+        <li class="drop-block2"><a href="#" >Shop Airpods</a></li>
+        <li class="drop-block2"><a href="#">Airpods accessories</a></li>
+        // <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Airpods</li>
+        <li class="drop-block3"><a href="#">Airpods support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for Headphones</a></li>
+        <li class="drop-block3"><a href="#">Apple Music</a></li>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = airp;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function tvhome() {
+    let tvho = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop TV & Home</li>
+        <li class="drop-block2"><a href="#" >Shop Apple TV 4k</a></li>
+        <li class="drop-block2"><a href="#" >Shop Homepod</a></li>
+        <li class="drop-block2"><a href="#" >Shop Homepod mini</a></li>
+        <li class="drop-block2"><a href="#" >Shop siri remote</a></li>
+        <li class="drop-block2"><a href="#">TV & Home Acesoriess</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Tv & Home</li>
+        <li class="drop-block3"><a href="#">Apple TV support</a></li>
+        <li class="drop-block3"><a href="#">Homepod support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ </a></li>
+        <li class="drop-block3"><a href="#">Apple tv app</a></li>
+        <li class="drop-block3"><a href="#">Apple TV+</a></li>
+        <li class="drop-block3"><a href="#">Home app</a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#"Mac for Business></a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = tvho;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function Entertainment() {
+    let enter = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Mac</li>
+        <li class="drop-block2"><a href="#" >Shop Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Mac</li>
+        <li class="drop-block3"><a href="#">Mac support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for Mac</a></li>
+        <li class="drop-block3"><a href="#">MacOS sonoma</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#"Continuity></a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#"Mac for Business></a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = enter;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function accessories() {
+    let acces = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Mac</li>
+        <li class="drop-block2"><a href="#" >Shop Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Mac</li>
+        <li class="drop-block3"><a href="#">Mac support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for Mac</a></li>
+        <li class="drop-block3"><a href="#">MacOS sonoma</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#"Continuity></a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#"Mac for Business></a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = acces;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+function support() {
+    let supp = `<div id="mac-box">
+    <ul id="store-block1">
+        <li id="block1-heading">Explore Mac</li>
+        <li class="drop-block1"><a href="#" >Explore All Mac</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">MacBook</a></li>
+        <li class="drop-block1"><a href="#">iMac</a></li>
+        <li class="drop-block1"><a href="#">Mac mini</a></li>
+        <li class="drop-block1"><a href="#">Mac Studio</a></li>
+        <li class="drop-block1"><a href="#">Mac Pro</a></li>
+        <li class="drop-block1" style="margin-bottom:15px;"><a href="#">Displays</a></li>
+        <li class="drop-block2"><a href="#">Compare Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac Does that</a></li>
+
+    </ul>
+    <ul id="store-block2">
+        <li id="block2-heading">Shop Mac</li>
+        <li class="drop-block2"><a href="#" >Shop Mac</a></li>
+        <li class="drop-block2"><a href="#">Mac accessories</a></li>
+        <li class="drop-block2"><a href="#">Ways to Buy</a></li>
+    </ul>
+    <ul id="store-block3">
+        <li id="block3-heading">More from Mac</li>
+        <li class="drop-block3"><a href="#">Mac support</a></li>
+        <li class="drop-block3"><a href="#">AppleCare+ for Mac</a></li>
+        <li class="drop-block3"><a href="#">MacOS sonoma</a></li>
+        <li class="drop-block3"><a href="#">Apps by Apple</a></li>
+        <li class="drop-block3"><a href="#"Continuity></a></li>
+        <li class="drop-block3"><a href="#">iCloud+</a></li>
+        <li class="drop-block3"><a href="#"Mac for Business></a></li>
+        <li class="drop-block3"><a href="#">Education</a></li>
+    </ul>
+</div>`
+    document.getElementById('dropmanu-box').innerHTML = supp;
+    document.getElementById('offer_info').style.display = "none";
+
+}
+
+
+function dropmanu_out() {
+    // document.getElementById('stor') = " ";
+    document.getElementById('dropmanu-box').innerHTML = "";
+    document.getElementById('offer_info').style.display = "block";
+
+}
+
+function drophide() {
+    let track = window.event.clientY;
+    if (track < 5) {
+        document.getElementById('dropmanu-box').innerHTML = "";
+        document.getElementById('offer_info').style.display = "block";
+    }
+}
